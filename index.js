8838729c132d637b01ea0aa356104364c59b45df
ianvianini <ianvianini@gmail.com>
2024-09-01 17:22:10 -0300

    Desafio usando Else If

---
+++ index.js
@@ -0,0 +1,40 @@
+//Ian
+
+let nome = "Ian"
+let exp = 9999999
+
+console.log("Bem vindo a guilda");
+
+if (exp <=1000)
+    {
+    console.log("Aventureiro "+ nome + " Está no nível Ferro")
+    }
+    else if (exp <= 2000)
+    {
+        console.log("Aventureiro "+ nome + " Está no nível Bronze")
+    }
+    else if (exp <= 5000)
+        {
+        console.log("Aventureiro " + nome + " Esta no nível Prata")
+    }
+    else if (exp <= 7000)
+        {
+        console.log("Aventureiro " + nome + " Esta no nível Ouro")
+    }
+    else if (exp <= 8000)
+        {
+        console.log("Aventureiro " + nome + " Esta no nível Platina")
+    }
+    else if (exp <= 9000)
+        {
+        console.log("Aventureiro " + nome + " Esta no nível Ascendente")
+    }
+    else if (exp <= 10000)
+        {
+        console.log("Aventureiro " + nome + " Esta no nível Imortal")
+    }
+    else
+    {
+        console.log("Aventureiro " + nome + " Esta no nível Radiante")
+    }
+    
